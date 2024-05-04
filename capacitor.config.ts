09908cf120c02c768d43b12993f055a90307dda6
Pierre-Henry Soria <pierre@ph7.me>
2024-05-04 20:28:36 +1000

    Add Capactiro Folders

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'me.ph7.succeedaisearch',
-  appName: 'SucceedAISearch',
-  webDir: 'dist'
+  appId: "me.ph7.succeedaisearch",
+  appName: "SucceedAISearch",
+  webDir: "www",
 };
 
 export default config;
