acaf0f2488524f3068cc8d37b0dcf3c85cb12337
Pierre-Henry Soria <pierre@ph7.me>
2024-05-04 20:02:44 +1000

    Implement CapacitorJS :rocket:

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'me.ph7.succeedaisearch',
+  appName: 'SucceedAISearch',
+  webDir: 'dist'
+};
+
+export default config;
