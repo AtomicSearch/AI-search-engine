5ec35fe4bb717e34481a24cc90b78fb3b21434f0
Pierre-Henry Soria <pierre@ph7.me>
2024-05-05 15:57:34 +1000

    Move Loading Spinner into a reusable atom component

---
+++ client/components/atoms/Loading.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const LoadingSpinner = styled.div`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 2px solid #ccc;
+  border-top-color: #333;
+  animation: spin 1s linear infinite;
+  margin-right: 8px;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
--- client/pages/search/SearchPage.tsx
+++ client/pages/search/SearchPage.tsx
@@ -18,31 +18,13 @@ import { search } from "../../modules/search";
 import { FooterInfo } from "../../components/FooterInfo";
 import { UpgradePlanModal } from "../../components/UpgradePlanModal";
 import styled from "styled-components";
+import { LoadingSpinner } from "../../components/atoms/Loading";
 
 const LoadingContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 20px;
-`;
-
-const LoadingSpinner = styled.div`
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
-  border: 2px solid #ccc;
-  border-top-color: #333;
-  animation: spin 1s linear infinite;
-  margin-right: 8px;
-
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
 `;
 
 const LoadingText = styled.span`
