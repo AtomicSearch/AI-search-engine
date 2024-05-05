4ca44e6f63ac8ecc29ce41912471f2590362dfd3
Pierre-Henry Soria <pierre@ph7.me>
2024-05-06 02:58:46 +1000

    add zindex to confetti speading

--- client/constants/confettiOptions.constant.ts
+++ client/constants/confettiOptions.constant.ts
@@ -4,10 +4,12 @@ type ConfettiOptions = {
   particleCount?: number;
   startVelocity?: number;
   angle?: number;
+  zIndex?: number;
 };
 
 export const confettiOptions: ConfettiOptions = {
   spread: 100,
   startVelocity: 35,
   origin: { y: 0.6 },
+  zIndex: 9999,
 };
