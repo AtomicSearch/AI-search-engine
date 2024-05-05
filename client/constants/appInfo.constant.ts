3b6533ad4a2f0ee3032f41b7fa499fb7d0c0b2ce
Pierre-Henry Soria <pierre@ph7.me>
2024-05-05 16:25:24 +1000

    Add Legal Links

--- client/constants/appInfo.constant.ts
+++ client/constants/appInfo.constant.ts
@@ -1,3 +1,13 @@
+export enum AppInfo {
+  APP_NAME = "Atomic Search",
+}
+
+export enum AppLegalLinks {
+  TERMS = "https://your-terms-url", // Your terms URL
+  PRIVACY = "https://your-privacy-url", // Your privacy URL
+  CONTACT_EMAIL = "contact@ph7.me",
+}
+
 export enum GitHubInfo {
   AUTHOR_GITHUB_URL = "https://github.com/pH-7",
   AUTHOR_GITHUB_HANDLE = "@pH-7",
