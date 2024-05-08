4ee80a7f10eb57a23a2750083d6626f91a7fc8f4
Pierre-Henry Soria <pierre@ph7.me>
2024-05-08 19:04:25 +1000

    Remove `getQuerySuggestions` from vite config. Now in `querySuggestions` For better readability and maintenance. `querySuggestions` has been to `modules/querySuggestions.ts` instead of defining the suggestions at build time
     Please enter the commit message for your changes. Lines starting

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 import { writeFileSync, readFileSync, existsSync } from "node:fs";
 import temporaryDirectory from "temp-dir";
 import path from "node:path";
-import fs from "node:fs";
 import {
   initModel,
   distance as calculateSimilarity,
@@ -31,7 +30,6 @@ export default defineConfig(({ command }) => {
     root: "./client",
     define: {
       VITE_SEARCH_TOKEN: JSON.stringify(getSearchToken()),
-      VITE_QUERY_SUGGESTIONS: getQuerySuggestions(50),
     },
     server: {
       host: process.env.HOST,
@@ -299,16 +297,6 @@ function regenerateSearchToken() {
 function getSearchToken() {
   if (!existsSync(getSearchTokenFilePath())) regenerateSearchToken();
   return readFileSync(getSearchTokenFilePath(), "utf8");
-}
-
-function getQuerySuggestions(limit?: number) {
-  return (
-    JSON.parse(
-      fs.readFileSync("gossip.query-suggestions.json").toString(),
-    ) as string[]
-  )
-    .sort(() => Math.random() - 0.5)
-    .slice(0, limit);
 }
 
 let embeddingModelInstance: EmbeddingsModel | undefined;
