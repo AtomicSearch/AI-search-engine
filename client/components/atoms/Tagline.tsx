1626f091618723eb2eac7ddbc7b156b9a8c2ec0d
Pierre-Henry Soria <pierre@ph7.me>
2024-05-11 21:56:39 +1000

    Keep Tagline block component be an atom
    Move away the taglines list into its own exported array in config folder

--- client/components/atoms/Tagline.tsx
+++ client/components/atoms/Tagline.tsx
@@ -3,6 +3,7 @@ import { FaSearch, FaSearchengin } from "react-icons/fa";
 
 import { AppInfo } from "../../constants/appInfo.constant";
 import { random } from "../../utils/random";
+import { taglines } from "../../../config/taglines";
 
 const TaglineContainer = styled.div`
   display: flex;
@@ -30,23 +31,6 @@ const TaglineText = styled.h2`
 `;
 
 const icons = [<SearchenginIcon />, <SearchIcon />];
-
-const taglines = [
-  AppInfo.APP_TAGLINE, // default principal tagline
-  "The knowledge for every decision",
-  "Rapid. Reliable. AI-first search engine",
-  "One-click search. No time wasted",
-  "Gives only the relevant URLs for all inquiries",
-  "Productivity efficiency guaranteed",
-  "The engine that saves people time",
-  "AI-powered search engine",
-  "Grab the information you need on the go",
-  "AI-first knowledge engine",
-  "Fast. Reliable. Naturally Smart",
-  "Time-saver for all your inquiries",
-  "Stop to generic results. Receive domain-specific answers",
-  "Reinventing how knowledge is distributed"
-];
 
 const pickedUpTagline = random(taglines);
 const pickedUpIcons = random(icons);
---
+++ config/taglines.ts
@@ -0,0 +1,19 @@
+import { AppInfo } from "../client/constants/appInfo.constant";
+
+export const taglines = [
+  AppInfo.APP_TAGLINE, // default principal tagline
+  "The knowledge for every decision",
+  "Rapid. Reliable. AI-first search engine",
+  "One-click search. No time wasted",
+  "Gives only the relevant URLs for all inquiries",
+  "Productivity efficiency guaranteed",
+  "The engine that saves people time",
+  "AI-powered search engine",
+  "Grab the information you need on the go",
+  "AI-first knowledge engine",
+  "Fast. Reliable. Naturally Smart",
+  "Time-saver for all your inquiries",
+  "Stop to generic results. Receive domain-specific answers",
+  "Reinventing how knowledge is distributed",
+  "Retrieve the relevant findings. AI knowledge-based search",
+];
