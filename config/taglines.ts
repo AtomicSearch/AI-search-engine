49ffcbd527f0a4983fd22f8137fd5dac48a6d3a2
Pierre-Henry Soria <pierre@ph7.me>
2024-05-12 03:48:53 +1000

    Update wordings/meta description

--- config/taglines.ts
+++ config/taglines.ts
@@ -12,8 +12,8 @@ export const taglines = [
   "Grab the information you need on the go",
   "AI-first knowledge engine",
   "Fast. Reliable. Naturally Smart",
-  "Time-saver for all your inquiries",
-  "Stop to generic results. Receive domain-specific answers",
+  "Fast answer. Time-saver for all inquiries",
+  "Stop generic results. Receive domain-specific answers",
   "Reinventing how knowledge is distributed",
   "Retrieve the relevant findings. AI knowledge-based search",
 ];
