80f9c150599535fb7e04cd679ffc305c17041d74
Pierre-Henry Soria <pierre@ph7.me>
2024-05-11 18:44:33 +1000

    Random taglines and homepage icons

--- client/components/atoms/Tagline.tsx
+++ client/components/atoms/Tagline.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
-import { FaSearchengin } from "react-icons/fa";
+import { FaSearch, FaSearchengin } from "react-icons/fa";
+
 import { AppInfo } from "../../constants/appInfo.constant";
+import { random } from "../../utils/random";
 
 const TaglineContainer = styled.div`
   display: flex;
@@ -9,7 +11,13 @@ const TaglineContainer = styled.div`
   margin-bottom: 20px;
 `;
 
-const TaglineIcon = styled(FaSearchengin)`
+const SearchIcon = styled(FaSearch)`
+  font-size: 20px;
+  margin-left: 8px;
+  color: #0066cc;
+`;
+
+const SearchenginIcon = styled(FaSearchengin)`
   font-size: 24px;
   margin-left: 8px;
   color: #0066cc;
@@ -21,10 +29,29 @@ const TaglineText = styled.h2`
   color: #333;
 `;
 
+const icons = [<SearchenginIcon />, <SearchIcon />];
+
+const taglines = [
+  AppInfo.APP_TAGLINE, // default principal tagline
+  "The knowledge for every decision",
+  "Rapid. Reliable. AI-first search engine",
+  "One-click search. No time wasted",
+  "Gives only the relevant URLs for all inquiries",
+  "Productivity efficiency guaranteed",
+  "The engine that saves people time",
+  "AI-powered search engine",
+  "Grab the information you need on the go",
+  "AI-first knowledge engine",
+  "Fast. Reliable. Naturally Smart",
+];
+
+const pickedUpTagline = random(taglines);
+const pickedUpIcons = random(icons);
+
 export const Tagline = () => {
   return (
     <TaglineContainer>
-      <TaglineText>{AppInfo.APP_TAGLINE}</TaglineText> <TaglineIcon />
+      <TaglineText>{pickedUpTagline}</TaglineText> {pickedUpIcons}
     </TaglineContainer>
   );
 };
