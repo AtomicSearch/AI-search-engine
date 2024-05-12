fc4f787446072c79fe77f03e2fdcbbc6a3a20ae9
Pierre-Henry Soria <pierre@ph7.me>
2024-05-12 18:43:39 +1000

    (WIP) Use a secure API call to check subscriptions. Dont use localStorage as user client can update/change it so easily

--- client/hooks/useSubscriptionStatus.tsx
+++ client/hooks/useSubscriptionStatus.tsx
@@ -9,5 +9,38 @@ export const useSubscriptionStatus = (): boolean => {
     setIsUserSubscribed(subscriptionStatus === SubscriptionPlan.ACTIVE_STATUS);
   }, []);
 
+  // TODO Make a secure API call to fetch the subscription status instead of using localStorage
+  // useEffect(() => {
+  //   const fetchSubscriptionStatus = async () => {
+  //     try {
+  //       // Make a secure API call to fetch the subscription status
+  //       const response = await fetch("/api/subscriptionStatus", {
+  //         method: "GET",
+  //         credentials: "same-origin", // Send cookies for authentication
+  //       });
+
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         const subscriptionStatus = data.status;
+
+  //         // Validate and sanitize the subscription status
+  //         if (subscriptionStatus === SubscriptionPlan.ACTIVE_STATUS) {
+  //           setIsUserSubscribed(true);
+  //         } else {
+  //           setIsUserSubscribed(false);
+  //         }
+  //       } else {
+  //         // Handle error case
+  //         setIsUserSubscribed(false);
+  //       }
+  //     } catch (error) {
+  //       // Handle error case
+  //       setIsUserSubscribed(false);
+  //     }
+  //   };
+
+  //   fetchSubscriptionStatus();
+  // }, []);
+
   return isUserSubscribed;
 };
